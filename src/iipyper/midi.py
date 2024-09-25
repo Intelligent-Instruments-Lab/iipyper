63e04c662b973e6802f62871b1da00d538a2f634
Victor Shepardson <victor.shepardson@gmail.com>
2024-09-25 18:18:30 +0000

    MIDI feedback suppression

--- src/iipyper/midi.py
+++ src/iipyper/midi.py
@@ -3,8 +3,10 @@ import functools as ft
 import time
 import traceback
 from typing import Optional, List, Union
+from collections import defaultdict
 
 import mido
+from mido.frozen import freeze_message
 
 from .state import _lock
 
@@ -52,8 +54,10 @@ class MIDI:
         in_ports:Optional[List[str]]=None, 
         out_ports:Optional[List[str]]=None, 
         virtual_in_ports:int=1, virtual_out_ports:int=1, 
-        verbose:int=1, 
+        suppress_feedback:bool=True,
+        suppress_feedback_window:float=1e-3,
         # sleep_time:float=5e-4
+        verbose:int=1, 
         ):
         """
         Args:
@@ -61,6 +65,8 @@ class MIDI:
             out_ports: list of output devices to open (uses none by default)
             virtual_in_ports: number of 'To iipyper X' ports to create
             virtual_out_ports: number of 'From iipyper X' ports to create
+            suppress_feedback: if True, ignore any MIDI message recently sent on an output port 
+            suppress_feedback_window: max delay in seconds to consider feedback
         """
         if not MIDI.ports_printed and verbose:
             MIDI.print_ports()
@@ -84,17 +90,13 @@ class MIDI:
         if in_ports is None or len(in_ports)==0:
             in_ports = set(mido.get_input_names())
 
+        # MIDI feedback suppression stuff
+        self.suppress_feedback = suppress_feedback
+        # map from MIDI event to list of timestamps
+        self.recent_outputs = defaultdict(list)
+        self.max_feedback_ns = suppress_feedback_window * 1e9
+
         self.in_ports = {}  
-        # for i in range(virtual_in_ports):
-        #     virtual_in = f'To iipyper {i+1}'
-        #     self.in_ports[virtual_in] = mido.open_input(
-        #         virtual_in, virtual=True)
-        # for port in in_ports:
-        #     try:
-        #         self.in_ports[port] = mido.open_input(
-        #             port, callback=self.get_callback(port))
-        #     except Exception:
-        #         print(f"""WARNING: MIDI input {port} not found""")
         for port in in_ports:
             cb = self.get_callback(port)
             try:
@@ -113,7 +115,6 @@ class MIDI:
         if self.verbose:
             print(f"""opened MIDI input ports: {list(self.in_ports)}""")
 
-        ##### WIP
         self.out_ports = {}
         for i in range(virtual_out_ports):
             port = f'From iipyper {i+1}'
@@ -124,9 +125,6 @@ class MIDI:
 
         if out_ports is None:
             out_ports = []
-        # if out_ports is None or len(out_ports)==0:
-            # out_ports = set(mido.get_output_names())  
-        # self.out_ports = {}
         for port in out_ports:
             try:
                 self.out_ports[port] = mido.open_output(port)
@@ -195,6 +193,21 @@ class MIDI:
     def get_callback(self, port_name):
         if self.verbose>1: print(f'handler for MIDI port {port_name}')
         def callback(msg):
+
+            # check recent outputs.
+            # if any are older than threshold, drop them
+            # if one is younger, drop it, break the search and ignore the input
+            if self.suppress_feedback:
+                m = freeze_message(msg)
+                t = time.time_ns()
+                ts = self.recent_outputs[m]
+                while len(ts):
+                    age = t - ts.pop()
+                    if age<self.max_feedback_ns:
+                        if self.verbose > 2:
+                            print(f'suppressing MIDI feedback {msg} port={port_name}')
+                        return
+
             if self.verbose > 1:
                 print(f'filtering MIDI {msg} port={port_name}')
             if not self.running:
@@ -229,11 +242,15 @@ class MIDI:
         """send on a specific port or all output ports"""
         ports = self.out_ports.values() if port is None else [self.out_ports[port]]
         # print(ports)
+        t = time.time_ns()
+        m = freeze_message(m)
         for p in ports:
             # print('iipyper send', m)
             # iiuc mido send should already be thread safe
             # with _lock:
             p.send(m)
+            self.recent_outputs[m].append(t)
+
 
     # # see https://mido.readthedocs.io/en/latest/message_types.html
 
